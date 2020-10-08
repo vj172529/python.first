4ba0098f8c3890f8cc79564a4d36c42704787744
Finlay Kilcullen <Fkilcullen@hotmail.com>
2020-10-08 11:11:57 +0100

    How are you question added

--- welcome.py
+++ welcome.py
@@ -1,2 +1,8 @@
 name = input("Enter Your Name: ")
 print("WELCOME TO THE WORLD OF PYTHON ", name)
+
+condition = input("How are you today? ")
+if condition == "good":
+    print("Great I am good too")
+elif condition == "bad":
+    print("Sorry to hear about that :(")
